893da517818bd540a00967489716c5027c9bc88d
gblbjj <oliveira992gabriel@gmail.com>
2019-06-20 18:35:21 -0300

    update system

---
+++ update-system/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+x=`who | awk '{print$1}'`
+
+while true; do 
+
+    if [ "$x" = "gblbjj"]
+    then 
+            echo "atualizando sistema aguade..."
+            apt-get update -y 
+    else 
+        echo "User não reconhecido" 
+    fi           
+
+done 
