86e2dfd1f4358d38a6c685c953b40c98610fdda3
gblbjj <oliveira992gabriel@gmail.com>
2019-06-20 18:10:38 -0300

    java

---
+++ squid/java/openjdk7.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function god () {
+
+x=`cat /etc/*-release | sed -n 1p | cut -d "=" -f2`
+
+}
+
+god 
+case "$x" in
+    Ubuntu)
+        echo "S.O reconhecido"
+        echo 
+        echo "inicinado instalação openjdk-7"
+        apt-get update -y    
+        echo 
+        apt-get install openjdk-7-jre -y 
+        echo
+        apt-get install openjdk-7-jdk -y 
+    ;;
+    CentOS)
+        echo "S.O não reconhecido"
+    ;;
+esac            
